6b1afe5a9faf647bc4570bd9a67cd3247aabad41
Apolo Moraes <vlogsapolo@gmail.com>
2023-01-06 16:28:17 -0300

    add knex and migrations

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
 
@@ -29,16 +29,15 @@ class UsersController {
       'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
       [name, email, hashedPassword]
     )
-
     return response.status(201).json()
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
-    const user = await database.get('SELECT * FROM users WHERE = (?)', [id])
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
 
     if (!user) {
       throw new AppError('Usuário não encontrado')
@@ -53,20 +52,36 @@ class UsersController {
       throw new AppError('Este e-mail já está em uso')
     }
 
-    user.name = name
-    user.email = email
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError(
+        'Você precisa informar a senha antiga para definir a nova senha'
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere.')
+      }
+
+      user.password = await hash(password, 8)
+    }
 
     await database.run(
       `UPDATE users SET
     name = ?,
     email = ?,
     password = ?,
-    updated_at = ?,
+    updated_at = DATETIME('now')
     WHERE id = ?`,
-      [user.name, user.email, user.password, new Date(), id]
+      [user.name, user.email, user.password, id]
     )
 
-    return response.status(200).json()
+    return response.json()
   }
 }
 
