86a558a0ca802401f177661c133064c25ae56cf3
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-28 16:43:37 -0300

    add routes avatar image

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -6,7 +6,7 @@ const crypto = require('crypto');
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 
 // onde a imagem de fato vai ficar.
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
   //para onde o arquivo vai ir.
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,19 +1,12 @@
 const { Router } = require('express')
+const multer = require("multer");
+const uploadConfig = require('../configs/upload');
 
 const UsersController = require('../controllers/UsersController');
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router()
-
-// function myMiddleware(request, response, next) {
-//   if (!request.body.isAdmin /* request*/) {
-//     return response.json({
-//       message: 'user não autorizado, você não é adm'
-//     }) /* response */
-//   }
-
-//   next() /*next*/
-// }
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController()
 
@@ -23,6 +16,10 @@ usersRoutes.post('/', /*myMiddleware,*/ usersController.create)
 //o middleware nesse caso está se aplicando para uma rota específica
 
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single("avatar"), (req, res) => {
+  console.log(req.file.filename);
+  res.json();
+})
 
 
 module.exports = usersRoutes
